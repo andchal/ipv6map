4357e2ca4856c251200300952b653bcd7a978c13
Andrew Haley <achaley22@gmail.com>
2018-08-29 20:33:25 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='interview',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='andrew',
+    author_email='achaley22@gmail.com',
+    description='A quick demo for showing IPv6 geolocation data in a heatmap.',
+    install_requires=['Flask', 'requests']
+)
